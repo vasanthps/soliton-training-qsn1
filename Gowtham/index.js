9ce6782ae32d5449a7917021ef59dafcb7c5f1e0
Gowtham <gowthamgg05@gmail.com>
2016-10-15 11:21:41 +0530

    Implement convert string to integer

--- Gowtham/index.js
+++ Gowtham/index.js
@@ -1,19 +1,47 @@
-// Fill the dots
+// Generate Missing Integers
+function generateIntegerArray(startIndex, endIndex){
+    var arr = new Array();
+    for (var i = startIndex+1; i < endIndex; i++) {
+        arr.push(i)
+    }
+    return arr;
+}
+
+// Convert Integer to String
+function convertStringToIntArr(stringArray){
+    var arr = new Array();
+    stringArray = stringArray.split(',');
+    for(var i = 0;i < stringArray.length;i++)
+    {
+        arr.push(parseInt(stringArray[i]));
+    }
+    return arr;
+}
+
+// Construct Integer array from missing strings 
 function constructArrayFromString(string){
-    console.log("Inside function");
-    console.log(string);
     string = string.replace(/\.+/g,'.')
-    console.log(string);
     var numStringsArr = string.split('.');
     var arrayLength = numStringsArr.length;
+    var resultArray = "";
+    var result = ""
     for (var i = 0; i < arrayLength-1; i++) {
-        console.log(numStringsArr[i]);
-        //Do something
-        var start = numStringsArr[i][numStringsArr[i].length-1];
-        var end = numStringsArr[i+1][0];
-        console.log(start + " " + end);
+        stringArr = numStringsArr[i].split(',')
+        stringNextArr = numStringsArr[i+1].split(',')
+        var start = stringArr[stringArr.length-1];
+        var end = stringNextArr[0];
+        var missingIntegers = generateIntegerArray(parseInt(start), parseInt(end));
+        //console.log(i + " missingIntegers" + missingIntegers);
+        resultArray = stringArr + ',' + missingIntegers;
+        result = result + resultArray + ',';
     }
+    result = result + numStringsArr[numStringsArr.length-1];
+    //console.log("Result " +result);
+    return convertStringToIntArr(result);
 }
 
 
-constructArrayFromString("1,2,3..6,7")
+//constructArrayFromString("1,2,3..6,7..9")
+console.log(constructArrayFromString("1,2,3..9,10,11...13...15.17"))
+//console.log(constructArrayFromString("11...13"))
+//console.log(generateIntegerArray(11, 13))
