55c3504731e1a7cb93d193c162989955b3173064
vairavan lakshmanan <vairavan lakshmanan>
2016-10-15 11:10:43 +0530

    My First Commit

---
+++ Vairavan/index.js
@@ -0,0 +1,14 @@
+var str = "1,2,3...6,7,8...11,12...15", i, j, len;
+num_array = str.split(",");
+res_array = [];
+len = num_array.length;
+for(i = 0; i < len; i += 1) {
+    if(num_array[i].length > 1){
+        for(j = parseInt(num_array[i].substring(0, num_array[i].indexOf('.'))); j <= parseInt(num_array[i].slice(4)); j += 1) {
+            res_array.push(j);
+        }
+    } else {
+        res_array.push(parseInt(num_array[i]));
+    }
+}
+console.log(res_array);
