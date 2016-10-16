b927c05f9b878b3bbee030b4cfd684b4d28998b6
Anoop Elias <anoopelias@gmail.com>
2016-10-16 23:01:35 +0530

    Simplified

--- Anoop/fill.js
+++ Anoop/fill.js
@@ -1,24 +1,17 @@
-var input = '1,2,4...6...11,12,13'
+var input = '1,2,4...6,7...11,12,13'
 
 var nums = input.split(',').reduce((arr, num) => {
-    if (num.indexOf('...') === -1) {
-        arr.push(parseInt(num));
-    } else {
+    let last = num.split('...')
+        .map(num => parseInt(num))
+        .reduce((begins, ends) => {
+            for (let i=begins; i<ends; i++) {
+                arr.push(i);
+            }
 
-        let last = num.split('...')
-            .map(num => parseInt(num))
-            .reduce((begins, ends) => {
-                for (let i=begins; i<ends; i++) {
-                    arr.push(i);
-                }
+            return ends;
+        });
 
-                return ends;
-            });
-
-        arr.push(last);
-
-    }
-
+    arr.push(last);
     return arr;
 }, []);
 
