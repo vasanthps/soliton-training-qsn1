2e7a0d111140bb2a373c8d059e3c389e2a98d3d2
Anoop Elias <anoopelias@gmail.com>
2016-10-16 22:51:50 +0530

    Handled case where ... appears on either side of the number

--- Anoop/fill.js
+++ Anoop/fill.js
@@ -1,17 +1,22 @@
-var input = '1,2,4...6,7,8...11,12,13'
+var input = '1,2,4...6...11,12,13'
 
 var nums = input.split(',').reduce((arr, num) => {
-    var index = num.indexOf('...');
-
-    if (index === -1) {
+    if (num.indexOf('...') === -1) {
         arr.push(parseInt(num));
     } else {
-        var begins = parseInt(num.substring(0, index));
-        var ends = parseInt(num.substring(index + 3));
 
-        for (let i=begins; i<=ends; i++) {
-            arr.push(i);
-        }
+        let last = num.split('...')
+            .map(num => parseInt(num))
+            .reduce((begins, ends) => {
+                for (let i=begins; i<ends; i++) {
+                    arr.push(i);
+                }
+
+                return ends;
+            });
+
+        arr.push(last);
+
     }
 
     return arr;
